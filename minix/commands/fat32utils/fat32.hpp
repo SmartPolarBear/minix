ab3dfaa0997df2c93e960dddb83d614e2074d43f
SmartPolarBear <clevercoolbear@outlook.com>
2019-02-07 21:19:09 +0800

    Utils

--- minix/commands/fat32utils/fat32.hpp
+++ minix/commands/fat32utils/fat32.hpp
@@ -1,100 +1,116 @@
-#pragma once
+#ifndef MINIX_COMMANDS_FAT32UTILS_FAT32_H_
+#define MINIX_COMMANDS_FAT32UTILS_FAT32_H_
 #include <string>
 #include <memory>
 #include <vector>
 #include <ctime>
 #include <exception>
 
-#define FAT32_MAX_NAME_LEN			256
+constexpr size_t FAT32_MAX_NAME_LEN = 256;
 
-namespace fat32 {
+namespace fat32
+{
 
-	template<typename T>
-	class maybe {
-	public:
-		bool is_some;
-		T value;
+template <typename T>
+class maybe
+{
+  public:
+	bool is_some;
+	T value;
 
-		maybe() : is_some(false) {
-		}
+	maybe() : is_some(false)
+	{
+	}
 
-		maybe(const T& value) : is_some(true), value(value) {
-		}
+	maybe(const T &value) : is_some(true), value(value)
+	{
+	}
 
-		maybe(T&& value) : is_some(true), value(std::move(value)) {
-		}
-	};
+	maybe(T &&value) : is_some(true), value(std::move(value))
+	{
+	}
+};
 
-	class exception : public std::exception {
-	private:
-		std::string str;
+class exception : public std::exception
+{
+  private:
+	std::string str;
 
-	public:
-		int ret;
-		exception(int _ret) : ret(_ret) {
-			str = "Return value: " + std::to_string(ret);
-		}
+  public:
+	int ret;
+	exception(int _ret) : ret(_ret)
+	{
+		str = "Return value: " + std::to_string(ret);
+	}
 
-		virtual const char* what() const throw() {
-			return str.c_str();
-		}
-	};
+	virtual const char *what() const throw()
+	{
+		return str.c_str();
+	}
+};
 
-	struct entry {
-		char filename[FAT32_MAX_NAME_LEN];
-		int is_directory;
-		int is_readonly;
-		int is_hidden;
-		int is_system;
+struct entry
+{
+	char filename[FAT32_MAX_NAME_LEN];
+	int is_directory;
+	int is_readonly;
+	int is_hidden;
+	int is_system;
 
-		// Only tm_mon, tm_mday, tm_year, tm_sec, tm_hour and tm_sec
-		// are set.
-		struct tm creation;
+	// Only tm_mon, tm_mday, tm_year, tm_sec, tm_hour and tm_sec
+	// are set.
+	struct tm creation;
 
-		// Only tm_mon, tm_mday and tm_year set.
-		struct tm access;
+	// Only tm_mon, tm_mday and tm_year set.
+	struct tm access;
 
-		// Only tm_mon, tm_mday, tm_year, tm_sec, tm_hour and tm_sec
-		// are set.
-		struct tm modification;
+	// Only tm_mon, tm_mday, tm_year, tm_sec, tm_hour and tm_sec
+	// are set.
+	struct tm modification;
 
-		int size_bytes;
-	};
+	int size_bytes;
+};
 
-	class file {
-	private:
-		friend class dir;
-		int handle;
-		int buf_size;
-		file(int _handle, int _buf_size) : handle(_handle), buf_size(_buf_size) {
-		}
-	
-	public:
-		maybe<std::vector<uint8_t>> read_block();
-		~file();
-	};
+class file
+{
+  private:
+	friend class dir;
+	int handle;
+	int buf_size;
+	file(int _handle, int _buf_size) : handle(_handle), buf_size(_buf_size)
+	{
+	}
 
-	class dir {
-	private:
-		friend class fs;
-		int handle;
-		int last_buf_size;
-		dir(int _handle) : handle(_handle) {}
+  public:
+	maybe<std::vector<uint8_t>> read_block();
+	~file();
+};
 
-	public:
-		maybe<entry> next_entry();
-		std::unique_ptr<dir> open_subdir();
-		std::unique_ptr<file> open_file();
-		~dir();
-	};
+class dir
+{
+  private:
+	friend class fs;
+	int handle;
+	int last_buf_size;
+	dir(int _handle) : handle(_handle) {}
 
-	class fs {
-	private:
-		int handle;
+  public:
+	maybe<entry> next_entry();
+	std::unique_ptr<dir> open_subdir();
+	std::unique_ptr<file> open_file();
+	~dir();
+};
 
-	public:
-		fs(std::string device);
-		std::unique_ptr<dir> open_root_dir();
-		~fs();
-	};
-}
+class fs
+{
+  private:
+	int handle;
+
+  public:
+	fs(std::string device);
+	std::unique_ptr<dir> open_root_dir();
+	~fs();
+};
+} // namespace fat32
+
+#endif
